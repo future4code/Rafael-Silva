df5f6807976fcb512e7febc921b5e408e0c7d096
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-06-15 15:51:31 -0300

    Exercicios da tarde aula 4

---
+++ semana1/operadores/index.js
@@ -0,0 +1,95 @@
+//Exercícios de interpretação de código
+
+//1.
+/*
+O primeiro console.log irar imprimir na tela: a. false
+O segundo console.log irar imprimir na tela: b. false
+O terceiro console.log irar imprimir na tela: c. true
+O quarto console.log irar imprimir na tela: d. boolean
+*/
+
+//2
+/*
+O retorno do prompt sempre será uma string,
+então irar juntar os dois textos.
+*/
+
+//3
+/*
+Para fazer uma soma basta converter com o Number(), assim:
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+*/
+
+// Exercícios de escrita de código
+
+//1
+// const idade = Number(prompt("Qual a sua idade?"))
+// const idadeBestFriend = Number(prompt("Qual a idade do seu melhor amigo ou amiga?"))
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?", idade > idadeBestFriend)
+
+// console.log("a diferença é:", idade - idadeBestFriend)
+
+//2
+// const par = Number(prompt("Insira um número par"))
+
+// console.log("O resto da divisão de", par, "por 2 é:", par % 2)
+
+/*
+Notei que qualquer número par ele retorna 0
+*/
+
+/*
+Se o usuário inserir um número ímpar ele retona 1
+*/
+
+//3
+
+// const idadeUser = Number(prompt("Digite a sua idade?"))
+
+// console.log("Sua idade em meses:", idadeUser * 12)
+// console.log("Sua idade em dias:", idadeUser * 31)
+// console.log("Sua idade em horas:", idadeUser * 60)
+
+//4
+
+// const firstNumber = Number(prompt("Digite um número:"))
+// const secondNumber = Number(prompt("Digite outro número:"))
+
+// console.log("O primeiro numero é maior que segundo?", firstNumber > secondNumber)
+// console.log("O primeiro numero é igual ao segundo?", firstNumber === secondNumber)
+
+// const divisivel1 = (firstNumber % secondNumber) === 0
+// const divisivel2 = (secondNumber % firstNumber) === 0
+
+
+// console.log("O primeiro numero é divisível pelo segundo?", divisivel1)
+// console.log("O segundo numero é divisível pelo primeiro?", divisivel2)
+
+//DESAFIO
+
+//1
+// let deFparaK = (77 - 32) * (5 / 9) + 273.15
+
+// console.log("A temperatura é: ", deFparaK, "K")
+
+// let deCparaF = (80) * (9 / 5) + 32
+
+// console.log("A temperatura é: ", deCparaF, "°F")
+
+// deCparaF = (30) * (9 / 5) + 32
+// deFparaK = (deCparaF - 32) * (5 / 9) + 273.15
+// console.log("A temperatura é: ", deCparaF, "°F")
+// console.log("A temperatura é: ", deFparaK, "K")
+
+// const temp = Number(prompt("Digite a temperatura em graus celsius:"))
+
+// deCparaF = (temp) * (9 / 5) + 32
+// deFparaK = (deCparaF - 32) * (5 / 9) + 273.15
+// console.log("A temperatura é: ", deCparaF, "°F")
+// console.log("A temperatura é: ", deFparaK, "K")
