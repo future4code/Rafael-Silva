b241888d3ee9e87e6e46cd20c18778b643f3f52b
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-06-28 13:20:18 -0300

    Exercício 13 - Completo

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -204,6 +204,15 @@ function filmeFavorito() {
 // EXERCÍCIO 13
 function imprimeChamada() {
     // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+    const filme = {
+        nome: "O Diabo Veste Prada",
+        ano: 2006,
+        diretor: "David Frankel",
+        atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+    }
+
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+
 }
 
 // EXERCÍCIO 14
