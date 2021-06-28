721df78d436da9777aa54d38b7d523d8d09c5477
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-06-28 13:09:57 -0300

    Exercício 12 - Completo

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -191,7 +191,14 @@ function ordenaArray(array) {
 
 // EXERCÍCIO 12
 function filmeFavorito() {
+    const filme = {
+        nome: "O Diabo Veste Prada",
+        ano: 2006,
+        diretor: "David Frankel",
+        atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+    }
 
+    return filme
 }
 
 // EXERCÍCIO 13
