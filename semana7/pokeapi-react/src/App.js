d31b64c731e5b93b608bec8f8e61db1e565d8477
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-07-29 14:10:45 -0300

    PokeAPI - Faltando tipos

---
+++ semana7/pokeapi-react/src/App.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import * as All from "./App.styles"
+import axios from "axios";
+
+class App extends React.Component {
+    state = {
+        aleatoryPokemon: [],
+        picturePokemon: "",
+        pokemon: []
+    }
+
+    componentDidMount() {
+        this.getPokemons().then()
+    }
+
+    getPokemons = async () => {
+        const url = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
+
+        try {
+            const response = await axios.get(url)
+            const aleatoryPokemon = Math.floor(Math.random() * response.data.results.length)
+            const pokemon = response.data.results.filter((pokemon, index) => {
+                return index === aleatoryPokemon
+            })
+
+            this.setState({
+                aleatoryPokemon: pokemon
+            })
+        } catch (e) {
+            alert(`Ooops! Ocorreu um erro. \n ${e.response}`)
+        }
+    }
+
+    getPicturePokemon = async () => {
+        let url
+        const pokemon = this.state.aleatoryPokemon.map((pokemon) => {
+            url = pokemon.url
+        })
+
+        try {
+            const response = await axios.get(url)
+            this.setState({
+                picturePokemon: response.data.sprites.front_default,
+                pokemon: [response.data]
+            })
+
+        } catch (e) {
+            alert(`Ooops! Ocorreu um erro. \n ${e.response}`)
+
+        }
+    }
+
+    showPokemon = () => {
+        this.getPokemons().then()
+        this.getPicturePokemon().then()
+    }
+
+    render() {
+        const pokemonCard = this.state.pokemon.map((pokemon) => {
+            return (
+                <All.Card key={pokemon.name}>
+                    {this.state.picturePokemon && <div>
+                        <All.CardCover>
+                            <img src={this.state.picturePokemon} alt="foto do pokemon"/>
+                        </All.CardCover>
+
+                        <All.CardContent>
+                            <div>
+                                <h3>Nome:</h3>
+                                <p>{pokemon.name}</p>
+                            </div>
+
+                            <div>
+                                <h3>Tipo:</h3>
+                                {pokemon.types.map((type) =>{
+                                    return (
+                                        <p>{type.name},</p>
+                                    )
+                                })}
+                                {/*<p>{console.log(pokemon.types)},</p>*/}
+                            </div>
+                        </All.CardContent>
+                    </div>
+                    }
+                </All.Card>
+            )
+        }
+    )
+        console.log(this.state.pokemon)
+
+    return (
+        <All.Container>
+            <All.HeaderContainer>
+                <All.Titles>
+                    <h2>Jornada no mundo de Pokémon</h2>
+                    <p>Você está explorando no mundo de pokémon, de repente você encontra um pokémon selvagem.</p>
+                    <p>Clique no botão abaixo para saber qual é esse pokémon!</p>
+                </All.Titles>
+
+                <button onClick={this.showPokemon}>Qual Pokémon?</button>
+            </All.HeaderContainer>
+
+            <All.MainContainer>
+                {pokemonCard}
+            </All.MainContainer>
+        </All.Container>
+    )
+    }
+    }
+
+    export default App;
---
+++ semana7/pokeapi-react/src/App.styles.js
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  //border: 1px solid #1D2025;
+  width: 100%;
+  flex-direction: column;
+`
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  border-bottom: 1px solid #1D2025;
+  width: 60%;
+  margin: 20px;
+  padding: 20px;
+
+  button {
+    padding: 5px;
+    border-radius: 5px;
+
+    :hover {
+      cursor: pointer;
+      color: #D36833;
+    }
+  }
+`
+
+export const Titles = styled.div`
+  display: flex;
+  //justify-content: flex-start;
+  //align-items: center;
+  flex-direction: column;
+  //border: 1px solid #1D2025;
+  padding: 20px;
+  margin: 10px auto;
+  text-align: left;
+
+  h2 {
+    font-weight: 600;
+    font-size: 30px;
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-size: 20px;
+    font-weight: 400;
+    margin-bottom: 7px;
+
+  }
+`
+
+
+export const MainContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  //border: 1px solid #1D2025;
+  width: 50%;
+  margin: 10px;
+  padding: 20px;
+`
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #1D2025;
+  padding: 20px;
+`
+
+export const CardCover = styled.div`
+  height: 150px;
+  width: 100%;
+
+  img {
+    width: 200px;
+  }
+`
+
+export const CardContent = styled.div`
+  margin-top: 50px;
+  flex: 1;
+  
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    p{
+      margin-left: 5px;
+    }
+  }
+`
