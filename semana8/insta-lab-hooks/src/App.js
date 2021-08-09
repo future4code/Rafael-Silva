c115e3a6a1e10c2e45d4a4d88d8c1af391961218
rafansilva <rafaelnascimento0505@gmail.com>
2021-08-09 15:32:46 -0300

    Implementações com Hooks feitas

---
+++ semana8/insta-lab-hooks/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Post from './components/Post/Post';
+import styled from "styled-components"
+
+const AppContainer = styled.div `
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px auto;
+`
+
+const App = () => {
+  return (
+    <AppContainer>
+      <Post
+        nomeUsuario={'Usuário'}
+        fotoUsuario={'https://picsum.photos/50/50'}
+        fotoPost={'https://picsum.photos/200/150'}
+      />
+    </AppContainer>
+  );
+}
+
+
+export default App;
