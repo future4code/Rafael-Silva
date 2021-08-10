94d94bdd3ecd4fd46ff1f32287f19cdb1b02b74d
rafansilva <rafaelnascimento0505@gmail.com>
2021-08-10 15:57:38 -0300

    inicio projeto

---
+++ semana9/astromatch/src/App.js
@@ -0,0 +1,12 @@
+//styles
+import {Container} from "./assets/App.styles";
+
+function App() {
+  return (
+    <Container>
+      <p>teste</p>
+    </Container>
+  );
+}
+
+export default App;
---
+++ semana9/astromatch/src/assets/App.styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  left: 0;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid #1D2025;
+`
