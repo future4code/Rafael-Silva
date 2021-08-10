12b05a913537719aad196c0a0d9c5b8f4c267dda
rafansilva <rafaelnascimento0505@gmail.com>
2021-08-10 18:49:56 -0300

    Estrutura Card pronto

--- semana9/astromatch/src/App.js
+++ semana9/astromatch/src/App.js
@@ -1,12 +1,52 @@
 //styles
-import {Container} from "./assets/App.styles";
+import {
+    Card,
+    CardContainer,
+    CardContent,
+    CardCover,
+    CardHeader,
+    Container,
+    Logo,
+    MainContainer,
+    Right
+} from "./assets/App.styles";
+import logo from "./assets/images/logo.png"
+import left from "./assets/images/left-arrow.png"
+import right from "./assets/images/right-arrow.png"
 
 function App() {
-  return (
-    <Container>
-      <p>teste</p>
-    </Container>
-  );
+    return (
+        <Container>
+            <CardContainer>
+                <CardHeader>
+
+                    <Logo src={logo} alt="logo astromatch"/>
+
+
+                    <Right src={right} alt=""/>
+
+                </CardHeader>
+
+
+                <MainContainer>
+                    <Card>
+                        <CardCover>
+                            <p>teste</p>
+                            <p>teste</p>
+
+                        </CardCover>
+
+                    </Card>
+
+                    <CardContent>
+                        <p>teste</p>
+
+                    </CardContent>
+                </MainContainer>
+
+            </CardContainer>
+        </Container>
+    );
 }
 
 export default App;
--- semana9/astromatch/src/assets/App.styles.js
+++ semana9/astromatch/src/assets/App.styles.js
@@ -6,11 +6,107 @@ export const Container = styled.div`
   top: 0;
   right: 0;
   left: 0;
-  
+
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   padding: 20px;
+`
+
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   border: 1px solid #1D2025;
+  width: 25%;
+  padding: 10px;
+
+  background-color: #FFF;
+`
+
+export const CardHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+
+  border-bottom: 1px solid #1D2025;
+`
+
+export const Logo = styled.img`
+  width: 30%;
+  margin: 0 auto;
+  cursor: pointer;
+`
+
+export const Right = styled.img`
+  width: 10%;
+  cursor: pointer;
+`
+
+
+export const MainContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid #1D2025;
+  width: 100%;
+  //margin: 10px;
+  //padding: 20px;
+`
+
+export const Card = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  border: 1px solid #1D2025;
+  padding: 20px;
+  width: 100%;
+`
+
+export const CardCover = styled.div`
+ 
+  height: 150px;
+  width: 100%;
+  
+
+  div
+  img {
+    width: 200px;
+  }
+`
+
+export const CardContent = styled.div`
+  margin-top: 50px;
+  flex: 1;
+
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    p {
+      margin-left: 5px;
+    }
+  }
+`
+
+export const Buttons = styled.div`
+  margin: 20px;
+
+  button {
+    margin-right: 10px;
+    padding: 5px;
+    border-radius: 5px;
+
+    :hover {
+      cursor: pointer;
+      color: #D36833;
+    }
+  }
 `
