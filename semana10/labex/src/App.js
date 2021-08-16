1bb521b368ca66a4be79ca478aa5ed7028a29280
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-08-16 16:53:38 -0300

    Inicio do projeto

---
+++ semana10/labex/src/App.js
@@ -0,0 +1,16 @@
+//Styles
+import {GlobalSettings} from "./assets/boot";
+
+function App() {
+    return (
+        <>
+            <title>LabeX - As Melhores Viagens Espaciais! </title>
+
+            <GlobalSettings>
+                <p>Olá</p>
+            </GlobalSettings>
+        </>
+    )
+}
+
+export default App;
---
+++ semana10/labex/src/assets/boot.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const GlobalSettings = styled.body`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    text-decoration: none;
+  }
+
+  width: 100vw;
+  height: 100vh;
+`
