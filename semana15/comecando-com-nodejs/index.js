ea0e5c2e44b3af625284e1286b6a6caaea6692d3
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-09-20 16:15:24 -0300

    update exercicio 2

--- semana15/comecando-com-nodejs/index.js
+++ semana15/comecando-com-nodejs/index.js
@@ -24,65 +24,67 @@ const green = chalk.green;
 
 // Exercicio 2:
 
-// const operator = process.argv[2];
+const operator = process.argv[2];
 
-// const firstNumber = Number(process.argv[3]);
-// const secondNumber = Number(process.argv[4]);
+const firstNumber = Number(process.argv[3]);
+const secondNumber = Number(process.argv[4]);
 
-// let result;
+let result;
 
-// switch (operator) {
-//     case "add":
-//         result = firstNumber + secondNumber;
+switch (operator) {
+    case "soma":
+        result = firstNumber + secondNumber;
 
-//         if (isNaN(result)) {
-//             console.log(
-//                 red("Ooops! Ocorreu um erro. Coloque add, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
-//             );
-//         } else {
-//             console.log(green(`O resultado da soma é: ${result}\n`));
-//         }
+        if (isNaN(result)) {
+            console.log(
+                red("Ooops! Ocorreu um erro. Coloque soma, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
+            );
+        } else {
+            console.log(green(`O resultado da soma é: ${result}\n`));
+        }
 
-//         break;
-//     case "sub":
-//         result = firstNumber - secondNumber;
+        break;
+    case "sub":
+        result = firstNumber - secondNumber;
 
-//         if (isNaN(result)) {
-//             console.log(
-//                 red("Ooops! Ocorreu um erro. Coloque add, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
-//             );
-//         } else {
-//             console.log(green(`O resultado da subtração é: ${result}\n`));
-//         }
+        if (isNaN(result)) {
+            console.log(
+                red("Ooops! Ocorreu um erro. Coloque soma, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
+            );
+        } else {
+            console.log(green(`O resultado da subtração é: ${result}\n`));
+        }
 
-//         break;
-//     case "mult":
-//         result = firstNumber * secondNumber;
+        break;
+    case "mult":
+        result = firstNumber * secondNumber;
 
-//         if (isNaN(result)) {
-//             console.log(
-//                 red("Ooops! Ocorreu um erro. Coloque add, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
-//             );
-//         } else {
-//             console.log(green(`O resultado da multiplicação é ${result}\n`));
-//         }
+        if (isNaN(result)) {
+            console.log(
+                red("Ooops! Ocorreu um erro. Coloque soma, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
+            );
+        } else {
+            console.log(green(`O resultado da multiplicação é ${result}\n`));
+        }
 
-//         break;
-//     case "div":
-//         result = firstNumber / secondNumber;
+        break;
+    case "div":
+        result = firstNumber / secondNumber;
 
-//         if (isNaN(result)) {
-//             console.log(
-//                 red("Ooops! Ocorreu um erro. Coloque add, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
-//             );
-//         } else {
-//             console.log(green(`O resultado da div é: ${result}\n`));
-//         }
+        if (isNaN(result)) {
+            console.log(
+                red("Ooops! Ocorreu um erro. Coloque soma, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
+            );
+        } else {
+            console.log(green(`O resultado da div é: ${result}\n`));
+        }
 
-//         break;
-//     default:
-//         console.log(
-//             red("Ooops! Ocorreu um erro. Coloque add, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
-//         );
-//         break;
-// }
+        break;
+    default:
+        console.log(
+            red("Ooops! Ocorreu um erro. Coloque soma, sub, mult ou div para realizar uma operação. Ex: div 100 2\n")
+        );
+        break;
+}
+
+
