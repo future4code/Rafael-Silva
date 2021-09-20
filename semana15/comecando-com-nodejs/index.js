8ffb8e4e43eaeadf9dcb40bc56ded16497934a0c
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-09-20 15:21:33 -0300

    exercicio completo

---
+++ semana15/comecando-com-nodejs/index.js
@@ -0,0 +1,17 @@
+// a) Para acessar os parâmetros passados na linha de comando, utilizamos o process.args[2]
+
+
+//b)
+// const userName = process.argv[2]
+// const userAge = process.argv[3]
+
+// console.log(`Olá, ${userName}! Você tem ${userAge} anos.`);
+
+//c)
+
+// const userName = process.argv[2]
+// const userAge = process.argv[3]
+
+// const futureAge = Number(userAge) + 7
+
+// console.log(`Olá, ${userName}! Você tem ${userAge} anos. Em sete anos você terá ${futureAge}`)
