fbd75cc5ed3cd2d0ec685ca5347a6a01a5e0ce40
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-10-01 18:10:27 -0300

    endpoint: editar usuário - Finalizado

--- semana16/todolist/src/App/app.ts
+++ semana16/todolist/src/App/app.ts
@@ -1,5 +1,8 @@
 import connection from "../Core/connection";
 
+//Types
+import { User } from "../Config/Types";
+
 // Create User
 export const createUser = async (id: number, name: string, nickname: string, email: string): Promise<any> => {
     try {
@@ -14,11 +17,36 @@ export const createUser = async (id: number, name: string, nickname: string, ema
     }
 };
 
+// Update Users
+export const updateUser = async (user: User): Promise<boolean> => {
+    try {
+        await connection("TodoListUser").update({
+          name: user.name,
+          nickname: user.nickname,
+          email: user.email
+        }).where({id: user.id});
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
 // Get User By id
 export const getUserById = async (id: number): Promise<any> => {
     try {
         const result = await connection.select("id", "nickname").from("TodoListUser").where({ id: id });
 
+        return result[0];
+    } catch (error) {
+        return false;
+    }
+};
+
+//Find User By id
+export const findUserById = async (id: number): Promise<User | boolean> => {
+    try {
+        const result = await connection.select("*").from("TodoListUser").where({ id: id });
         return result[0];
     } catch (error) {
         return false;
--- semana16/todolist/src/index.ts
+++ semana16/todolist/src/index.ts
@@ -2,7 +2,8 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
 import { uuid } from "./Config/Helpers";
-import { createUser, getUserById } from "./App/app";
+import { createUser, findUserById, getUserById, updateUser } from "./App/app";
+import { User } from "./Config/Types";
 
 const app: Express = express();
 
@@ -20,7 +21,7 @@ app.get("/user/:id", async (req: Request, res: Response) => {
         }
 
         const user = await getUserById(id);
-        
+
         if (user === false) {
             res.statusCode = 404;
             throw new Error("Usuário não encontrado.");
@@ -48,6 +49,53 @@ app.post("/user", async (req: Request, res: Response) => {
 
         await createUser(id, name, nickname, email);
 
+        res.status(201).send({ message: "Usuário criado com sucesso!" });
+    } catch (e) {
+        const error = e as Error;
+        console.log(error);
+        res.send({ message: error.message });
+    }
+});
+
+// Endpoint: Editar usuário
+app.put("/user/edit/:id", async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id);
+        const { name, nickname, email } = req.body;
+
+        if (!id) {
+            res.statusCode = 404;
+            throw new Error("Usuário não encontrado.");
+        }
+
+
+        const findUser = await findUserById(id);
+
+        if (findUser === false) {
+            res.statusCode = 404;
+            throw new Error("Usuário não encontrado.");
+        }
+
+        if (!name || !nickname) {
+            res.statusCode = 403;
+            throw new Error("Campos Inválidos!");
+        }
+
+        const newUser: User = {
+            id,
+            name,
+            nickname,
+            email
+        };
+
+        const user = await updateUser(newUser);
+
+        if (user === false) {
+            res.statusCode = 404;
+            throw new Error("Usuário não encontrado.");
+        } else {
+            res.status(200).send({ message: "Usuário atualizado com sucesso!" });
+        }
         res.status(201).send({ message: "Usuário criado com sucesso!" });
     } catch (e) {
         const error = e as Error;
