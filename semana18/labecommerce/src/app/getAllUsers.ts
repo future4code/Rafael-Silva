95d83f8dc001d4f8479ef6061c42a5d0db6b8608
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-10-16 17:59:58 -0300

    refactor: endpoint Get All Users

--- semana18/labecommerce/src/app/getAllUsers.ts
+++ semana18/labecommerce/src/app/getAllUsers.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from 'express';
 
 import UserDatabase from '../core/UserDatabase';
+import Message from '../support/Message';
 
 const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     try {
         const users = await new UserDatabase().getAllUsers();
-        res.status(200).send(users);
+        if (users === false) {
+            throw new Message(
+                'Ooops! Ocorreu um erro inesperado. Tente novamente mais tarde.',
+                400,
+            );
+        } else {
+            res.status(200).send(users);
+        }
     } catch (e) {
         const error = e as Error;
         // eslint-disable-next-line no-console
