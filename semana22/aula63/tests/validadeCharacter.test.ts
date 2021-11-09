37a6889ccd6a3aed74ba3ec8e0ea83d0a10153ac
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-11-09 16:53:31 -0300

    tes: Should return false if the character strength and defense is 0

--- semana22/aula63/tests/validadeCharacter.test.ts
+++ semana22/aula63/tests/validadeCharacter.test.ts
@@ -26,4 +26,26 @@ describe("Validating Characters", () => {
 
         expect(new UserBusiness().validateCharacter(character)).toBe(false);
     });
+
+    test("Should return false if the character defense is 0", () => {
+        const character: Character = {
+            name: "Test",
+            life: 100,
+            defense: 0,
+            strength: 90
+        };
+
+        expect(new UserBusiness().validateCharacter(character)).toBe(false);
+    });
+
+    test("Should return false if the character strength is 0", () => {
+        const character: Character = {
+            name: "Test",
+            life: 100,
+            defense: 10,
+            strength: 0
+        };
+
+        expect(new UserBusiness().validateCharacter(character)).toBe(false);
+    });
 });
