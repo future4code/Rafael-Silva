34c6a20e0fe906c35c4aceccdcdde33fa727a7a2
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-11-10 16:11:44 -0300

    refactor: add dependency injection, changed the order of the ErrorMessage class and instace UserModel for create newUser

--- semana22/aula64/src/business/UserBusiness.ts
+++ semana22/aula64/src/business/UserBusiness.ts
@@ -1,15 +1,26 @@
-import { UserData } from '../data/UserData';
-import Auth from '../services/TokenGenerator';
+
 import ErrorMessage from '../errors/ErrorMessage';
+import UserModel, { USER_ROLES } from '../models/UserModel';
+import { isEmail, passwdLength } from '../utils/helpers';
+import Auth from '../services/TokenGenerator';
+import IdGenerator from '../services/IdGenerator';
+import HashGenerator from '../services/HashManager';
+import UserData from '../data/UserData';
 
 export class UserBusiness {
+    constructor(
+        private idGenerator: IdGenerator,
+        private hashGenerator: HashGenerator,
+        private tokenGenerator: Auth,
+        private userData: UserData
+    ) { }
 
     public signupBusiness = async (input: any): Promise<object> => {
         const { name, email, password } = input;
         let { role } = input;
 
         if (!name || !email || !password) {
-            throw new ErrorMessage(400, "Todos os campos são obrigatórios");
+            throw new ErrorMessage(422, "Todos os campos são obrigatórios");
         }
 
         if (!role) {
@@ -17,48 +28,45 @@ export class UserBusiness {
         }
 
         if (!isEmail(email)) {
-            throw new ErrorMessage('`email` Inválido.', 400);
+            throw new ErrorMessage(422, "'email' Inválido.");
         }
 
-        if (
-            password.toString().length < Number(process.env.PASSWD_MIN) ||
-            password.toString().length > Number(process.env.PASSWD_MAX)
-        ) {
+        if (passwdLength(password)) {
             throw new ErrorMessage(
-                'É necessário um `password` entre 8 e 40 caracteres.',
-                400
+                400,
+                "É necessário um 'password' entre 8 e 40 caracteres."
             );
         }
 
         role = role.toUpperCase();
         if (!(role in USER_ROLES)) {
-            throw new ErrorMessage('`role` Inválido. É possível criar somente users `ADMIN` e `NORMAL`.', 400);
+            throw new ErrorMessage(400, "'role' Inválido. É possível criar somente users 'ADMIN' e 'NORMAL'.");
         }
 
-        const user = await UserData.findByEmail(email);
+        const user = await this.userData.findByEmail(email);
 
         if (user) {
-            throw new ErrorMessage('Email já cadastrado.', 401);
+            throw new ErrorMessage(401, "Email já cadastrado.");
         }
 
-        const id = uuid();
+        const id = this.idGenerator.generate();
 
-        const newUser: UserInterface = {
+        const newUser = new UserModel(
             id,
             name,
             email,
-            password: passwd(password),
+            this.hashGenerator.generateHash(password),
             role,
-        };
+        );
 
-        const result = await UserData.create(newUser);
+        const result = await this.userData.create(newUser);
 
-        const token = Auth.generateToken({ id, role });
+        const token = this.tokenGenerator.generate({ id, role });
 
         if (result === false) {
             throw new ErrorMessage(
-                'Oops! Ocorreu um error inesperado. Tente novamente mais tarde',
-                400
+                400,
+                'Oops! Ocorreu um error inesperado. Tente novamente mais tarde'
             );
         } else {
             return {
@@ -68,85 +76,85 @@ export class UserBusiness {
         }
     };
 
-    public loginBusiness = async (input: any): Promise<object> => {
-        const { email, password } = input;
+    // public loginBusiness = async (input: any): Promise<object> => {
+    //     const { email, password } = input;
 
-        if (!email || !password) {
-            throw new ErrorMessage('Todos os campos são obrigatórios', 400);
-        }
+    //     if (!email || !password) {
+    //         throw new ErrorMessage('Todos os campos são obrigatórios', 400);
+    //     }
 
-        if (!isEmail(email)) {
-            throw new ErrorMessage('`email` Inválido.', 400);
-        }
+    //     if (!isEmail(email)) {
+    //         throw new ErrorMessage('`email` Inválido.', 400);
+    //     }
 
-        const user = await UserData.findByEmail(email) as UserInterface;
+    //     const user = await UserData.findByEmail(email) as UserInterface;
 
-        if (!user) {
-            throw new ErrorMessage('Usuário não encontrado.', 401);
-        }
+    //     if (!user) {
+    //         throw new ErrorMessage('Usuário não encontrado.', 401);
+    //     }
 
-        if (!isPasswd(password, user.password)) {
-            throw new ErrorMessage('`email` ou `senha` Inválidos.', 401);
-        }
+    //     if (!isPasswd(password, user.password)) {
+    //         throw new ErrorMessage('`email` ou `senha` Inválidos.', 401);
+    //     }
 
-        const token = Auth.generateToken({ id: user.id, role: user.role });
+    //     const token = Auth.generateToken({ id: user.id, role: user.role });
 
-        return {
-            token
-        };
-    };
+    //     return {
+    //         token
+    //     };
+    // };
 
-    public getAllUser = async (token: string): Promise<UserInterface[] | boolean> => {
-        const tokenVerify = Auth.getTokenData(token) as AuthInterface;
+    // public getAllUser = async (token: string): Promise<UserInterface[] | boolean> => {
+    //     const tokenVerify = Auth.getTokenData(token) as AuthInterface;
 
-        if (!tokenVerify) {
-            throw new ErrorMessage(
-                "Token inválido, expirado ou ausente da chave 'token' do cabeçalho",
-                403
-            );
-        }
+    //     if (!tokenVerify) {
+    //         throw new ErrorMessage(
+    //             "Token inválido, expirado ou ausente da chave 'token' do cabeçalho",
+    //             403
+    //         );
+    //     }
 
-        const user = await UserData.findAll();
+    //     const user = await UserData.findAll();
 
-        if (!user) {
-            throw new ErrorMessage('Usuário não encontrado.', 401);
-        }
+    //     if (!user) {
+    //         throw new ErrorMessage('Usuário não encontrado.', 401);
+    //     }
 
-        return user;
-    };
+    //     return user;
+    // };
 
-    public deleteUser = async (token: string, id: string): Promise<object> => {
-        const tokenVerify = Auth.getTokenData(token) as AuthInterface;
+    // public deleteUser = async (token: string, id: string): Promise<object> => {
+    //     const tokenVerify = Auth.getTokenData(token) as AuthInterface;
 
-        if (!tokenVerify) {
-            throw new ErrorMessage(
-                "Token inválido, expirado ou ausente da chave 'token' do cabeçalho",
-                403
-            );
-        }
+    //     if (!tokenVerify) {
+    //         throw new ErrorMessage(
+    //             "Token inválido, expirado ou ausente da chave 'token' do cabeçalho",
+    //             403
+    //         );
+    //     }
 
-        if (tokenVerify.role !== USER_ROLES.ADMIN) {
-            throw new ErrorMessage('Apenas usuários `ADMIN` podem deletar usuários.', 403);
-        }
+    //     if (tokenVerify.role !== USER_ROLES.ADMIN) {
+    //         throw new ErrorMessage('Apenas usuários `ADMIN` podem deletar usuários.', 403);
+    //     }
 
-        const user = await UserData.findById(id);
+    //     const user = await UserData.findById(id);
 
-        if (!user) {
-            throw new ErrorMessage('Usuário não encontrado.', 401);
-        }
+    //     if (!user) {
+    //         throw new ErrorMessage('Usuário não encontrado.', 401);
+    //     }
 
-        const result = await UserData.delete(id);
+    //     const result = await UserData.delete(id);
 
-        if (result === false) {
-            throw new ErrorMessage(
-                'Oops! Ocorreu um error inesperado. Tente novamente mais tarde',
-                400
-            );
-        } else {
-            return {
-                message: 'Usuário deletado com sucesso!',
-            };
-        }
-    }
+    //     if (result === false) {
+    //         throw new ErrorMessage(
+    //             'Oops! Ocorreu um error inesperado. Tente novamente mais tarde',
+    //             400
+    //         );
+    //     } else {
+    //         return {
+    //             message: 'Usuário deletado com sucesso!',
+    //         };
+    //     }
+    // };
 
 }
